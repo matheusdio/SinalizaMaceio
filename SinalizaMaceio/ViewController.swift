46ba8d105e0613630ac8159c10e3428fab1241c7
Matheus <dio.nisio@live.com>
2024-06-30 13:19:45 -0300

    Initial Commit

---
+++ SinalizaMaceio/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SinalizaMaceio
+//
+//  Created by Matheus Dionísio on 30/06/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
