d9bc9e7018113aec7fea4a575ff4ef15ced46a8d
Matheus <dio.nisio@live.com>
2024-10-10 19:45:13 -0300

    Melhoria nos detalhes visuais 1.0

--- SinalizaMaceio/View/MapViewController.swift
+++ SinalizaMaceio/View/MapViewController.swift
@@ -12,9 +12,17 @@ import CoreLocation
 import TomTomSDKCommon
 import TomTomSDKReverseGeocoder
 import TomTomSDKReverseGeocoderOnline
+import TomTomSDKNavigationEngines
+import TomTomSDKRoute
+import TomTomSDKSearch
+import TomTomSDKSearchOnline
 
 class MapViewController: UIViewController {
 
+    var cardList: [Card] = []
+    var selectedCardList: Card?
+    let onlineSearch = OnlineSearchFactory.create(apiKey: "jZjWA6XkfKqKmGwn4hLhPWcUjsBnTWBG")
+    
     private lazy var container: UIView = {
         let view = UIView()
         view.translatesAutoresizingMaskIntoConstraints = false
@@ -26,7 +34,7 @@ class MapViewController: UIViewController {
         
         let view = UIView()
         view.translatesAutoresizingMaskIntoConstraints = false
-        view.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        view.backgroundColor = #colorLiteral(red: 0.9333333373, green: 0.9333333373, blue: 0.9333333373, alpha: 1)
         view.layer.cornerRadius = 24
         
         return view
@@ -54,6 +62,36 @@ class MapViewController: UIViewController {
         
         return collectionView
     }()
+    
+    private lazy var cardCollectionView: UICollectionView = {
+        
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        layout.minimumLineSpacing = 10
+        layout.minimumInteritemSpacing = 10
+        
+        let collectionView = UICollectionView(frame: CGRect.zero, collectionViewLayout: UICollectionViewFlowLayout())
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.backgroundColor = .none
+        collectionView.collectionViewLayout = layout
+        collectionView.showsHorizontalScrollIndicator = false
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        collectionView.register(CardCollectionViewCell.self, forCellWithReuseIdentifier: "CardCollectionViewCell")
+        
+        return collectionView
+    }()
+    
+    lazy var viewButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Visualizar", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.layer.cornerRadius = 12
+        button.clipsToBounds = true
+        addGradientToButton(button)
+        return button
+    }()
 
 
     override func viewDidLoad() {
@@ -65,6 +103,9 @@ class MapViewController: UIViewController {
         }
         mapZoomCollectionView.isUserInteractionEnabled = true
         view.layoutIfNeeded()
+        addGradientToButton(viewButton)
+        createNewCard()
+        cardCollectionView.reloadData()
     }
     
     private func setInitialCameraPosition(map: TomTomMap) {
@@ -74,16 +115,40 @@ class MapViewController: UIViewController {
         map.moveCamera(cameraUpdate)
     }
     
+    private func addGradientToButton(_ button: UIButton) {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = button.bounds
+        gradientLayer.colors = [UIColor.darkGray.cgColor, UIColor.white.cgColor]
+        gradientLayer.startPoint = CGPoint(x: 0, y: 0)
+        gradientLayer.endPoint = CGPoint(x: 1, y: 1)
+        
+        button.layer.sublayers?.removeAll(where: { $0 is CAGradientLayer })
+        button.layer.insertSublayer(gradientLayer, at: 0)
+    }
+    
+    func createNewCard() {
+        
+        let card1: Card = Card(street: "Menino Marcelo", neighborhood: "Serraria", condition: "Bom Estado", lastMaintenance: "13/09/2024", conditionImage: UIImage(named: "bomestado"))
+        let card2: Card = Card(street: "Fernandes Lima", neighborhood: "Farol", condition: "Parcialmente Desgastada", lastMaintenance: "12/06/2024", conditionImage: UIImage(named: "mauestado"))
+        let card3: Card = Card(street: "Alvaro Otacilio", neighborhood: "Ponta Verde", condition: "Bom estado", lastMaintenance: "20/08/2024", conditionImage: UIImage(named: "mauestado"))
+        
+        cardList.append(card1)
+        cardList.append(card2)
+        cardList.append(card3)
+    }
     
     private func setupViewHierarchy() {
+        
         view.addSubview(container)
         container.addSubview(mapView)
         view.addSubview(mapZoomCollectionView)
         view.addSubview(cardView)
-
+        view.addSubview(cardCollectionView)
+        view.addSubview(viewButton)
     }
     
     private func setupConstraints() {
+        
         NSLayoutConstraint.activate([
             container.topAnchor.constraint(equalTo: view.topAnchor),
             container.leadingAnchor.constraint(equalTo: view.leadingAnchor),
@@ -103,7 +168,17 @@ class MapViewController: UIViewController {
             cardView.bottomAnchor.constraint(equalTo: mapView.bottomAnchor),
             cardView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             cardView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            cardView.heightAnchor.constraint(equalToConstant: 300)
+            cardView.heightAnchor.constraint(equalToConstant: 280),
+            
+            viewButton.bottomAnchor.constraint(equalTo: cardView.bottomAnchor, constant: -30),
+            viewButton.leadingAnchor.constraint(equalTo: cardView.leadingAnchor, constant: 40),
+            viewButton.trailingAnchor.constraint(equalTo: cardView.trailingAnchor, constant: -40),
+            viewButton.heightAnchor.constraint(equalToConstant: 50),
+            
+            cardCollectionView.topAnchor.constraint(equalTo: cardView.topAnchor, constant: 20),
+            cardCollectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30),
+            cardCollectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            cardCollectionView.heightAnchor.constraint(equalToConstant: 150)
         ])
     }
 }
@@ -111,38 +186,77 @@ class MapViewController: UIViewController {
 extension MapViewController: UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 3
+        if collectionView == mapZoomCollectionView {
+            
+            return 3
+        } else if collectionView == cardCollectionView {
+            
+            return cardList.count
+        }
+        return 0
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ZoomCollectionViewCell.reuseIdentifier, for: indexPath) as! ZoomCollectionViewCell
-        cell.configure(for: indexPath.row)
-        return cell
+        if collectionView == mapZoomCollectionView {
+            
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ZoomCollectionViewCell.reuseIdentifier, for: indexPath) as! ZoomCollectionViewCell
+            cell.configure(for: indexPath.row)
+            
+            return cell
+            
+        } else if collectionView == cardCollectionView {
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CardCollectionViewCell", for: indexPath) as! CardCollectionViewCell
+            
+            cell.setupLayout()
+            cell.container.backgroundColor = UIColor.lightGray
+            cell.streetLabel.text = cardList[indexPath.item].street
+            cell.neighborhoodLabel.text = cardList[indexPath.item].neighborhood
+            cell.conditionLabel.text = cardList[indexPath.item].condition
+            cell.lastMaintenanceLabel.text = cardList[indexPath.item].lastMaintenance
+            cell.optionsImageView.image = cardList[indexPath.item].conditionImage
+            cell.container.backgroundColor = #colorLiteral(red: 0.9999999404, green: 1, blue: 1, alpha: 1)
+            
+            return cell
+        }
+        return UICollectionViewCell()
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: 60, height: 25)
+        
+        if collectionView == mapZoomCollectionView {
+            return CGSize(width: 60, height: 25)
+            
+        } else if collectionView == cardCollectionView {
+            return CGSize(width: 270, height: 140)
+        }
+        
+        return CGSize.zero
     }
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        
-        let zoomLevels: [Double] = [12, 13, 17]
-        let zoomLevel = zoomLevels[indexPath.row]
-        
-        mapView.getMapAsync { map in
-            let position = CLLocationCoordinate2D(latitude: -9.66599, longitude: -35.73528)
-            let cameraUpdate = CameraUpdate(position: position, zoom: zoomLevel)
-            let animationDuration: TimeInterval = 0.4
+        if collectionView == mapZoomCollectionView {
+           
+            let zoomLevels: [Double] = [12, 13, 17]
+            let zoomLevel = zoomLevels[indexPath.row]
             
-            map.applyCamera(cameraUpdate, animationDuration: animationDuration) { success in
-                if success {
-                    print("Animação concluída com sucesso!")
-                } else {
-                    print("Falha na animação.")
+            mapView.getMapAsync { map in
+                let position = CLLocationCoordinate2D(latitude: -9.66599, longitude: -35.73528)
+                let cameraUpdate = CameraUpdate(position: position, zoom: zoomLevel)
+                let animationDuration: TimeInterval = 0.4
+                
+                map.applyCamera(cameraUpdate, animationDuration: animationDuration) { success in
+                    if success {
+                        print("Animação concluída com sucesso!")
+                    } else {
+                        print("Falha na animação.")
+                    }
                 }
             }
+        } else if collectionView == cardCollectionView {
+            let cell = collectionView.cellForItem(at: indexPath) as! CardCollectionViewCell
+            let isCurrentlyVisible = !cell.lastMaintenanceLabel.isHidden
+            
+            cell.toggleLastMaintenanceVisibility(!isCurrentlyVisible)
         }
     }
 }
-
-
